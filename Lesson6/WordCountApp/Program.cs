89c1d83fccd7225fc9174456fc68aa5b7be2d8f4
Евгений <evg92faut@gmail.com>
2024-12-03 21:55:32 +0700

    finalize WordCount app

---
+++ Lesson6/WordCountApp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using WordCounterApp.FileProcessing;
+
+namespace WordCounterApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            string filePath = "D:\\Курс Rubius\\RubiusHomeWork\\Lesson6\\WordCountApp\\files\\War and Peace.txt";
+
+            try
+            {
+                // Чтение всего файла
+                int wordCountFullRead = WordCounter.CountWordsByFullRead(filePath);
+                Console.WriteLine($"Количество слов (чтение всего файла): {wordCountFullRead}");
+
+                // Потоковая обработка файла
+                int wordCountStreamRead = WordCounter.CountWordsByStream(filePath);
+                Console.WriteLine($"Количество слов (потоковая обработка): {wordCountStreamRead}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Lesson6/WordCountApp/Utils/RegexUtils.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WordCounterApp.Utils
+{
+    public static class RegexUtils
+    {
+       
+        public static int CountWords(string text)
+        {
+            var matches = Regex.Matches(text, @"\b\w+\b");
+            return matches.Count;
+        }
+    }
+}
