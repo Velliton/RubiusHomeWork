6d7b2f0309763d8fb9068d88b2436b3f39cd0b78
Евгений <evg92faut@gmail.com>
2024-12-09 22:15:47 +0700

    Counter App

---
+++ Lesson8/Counter/Counter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    public delegate void CounterEventHandler();
+
+    public class Counter
+    {
+        public event CounterEventHandler OnThresholdReached;
+
+        public void StartCounting()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine($"Счёт: {i}");
+                if (i == 77)
+                {
+                    OnThresholdReached?.Invoke();
+                }
+            }
+        }
+    }
+}
---
+++ Lesson8/Counter/Handler1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    public class Handler1
+    {
+        public void ReactToEvent()
+        {
+            Console.WriteLine("Пора действовать, ведь уже 77");
+        }
+    }
+}
